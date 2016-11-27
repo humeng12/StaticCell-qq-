d7c2cfcc08d5505ee728d7f8ffeb1f3342dfb33b
humeng12 <936608021@qq.com>
2016-11-27 18:03:41 +0800

    提交
    项目创建

---
+++ StaticCell(qq)/StaticCell(qq)Tests/StaticCell_qq_Tests.m
@@ -0,0 +1,39 @@
+//
+//  StaticCell_qq_Tests.m
+//  StaticCell(qq)Tests
+//
+//  Created by 胡猛 on 2016/11/27.
+//  Copyright © 2016年 HuMeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StaticCell_qq_Tests : XCTestCase
+
+@end
+
+@implementation StaticCell_qq_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
